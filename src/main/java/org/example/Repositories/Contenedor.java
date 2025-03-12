3a2755793500e36c86d742c1f95c5d72b69d972d
fran <franciscoquirogax@gmail.com>
2025-03-12 11:06:31 -0300

    Avance en clase

--- src/main/java/org/example/Repositories/Contenedor.java
+++ src/main/java/org/example/Repositories/Contenedor.java
@@ -3,10 +3,13 @@ package org.example.Repositories;
 import org.example.Exceptions.ContenedorVacioException;
 import org.example.Exceptions.FormatoInvalidoException;
 import org.example.Exceptions.IdentificadorDuplicadoException;
+import org.example.Model.Clases.Expansion;
 import org.example.Model.Clases.Genero;
+import org.example.Model.Clases.Juego;
 import org.example.Model.Clases.Producto;
 
 
+import java.security.cert.Extension;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -40,15 +43,16 @@ public class Contenedor<T extends Producto> {
         }
         return "La lista esta vacia";
     }
-    public String mostrarContenedor () throws ContenedorVacioException{
+
+    public String mostrarContenedor() throws ContenedorVacioException {
         StringBuilder mensaje = null;
         int contador = 0;
-        if(contenedor.isEmpty()) {
+        if (contenedor.isEmpty()) {
             throw new ContenedorVacioException("El contenedor esta vacio.");
         } else {
             mensaje = new StringBuilder("Elementos en el contenedor: ");
         }
-        for(T elemento : contenedor) {
+        for (T elemento : contenedor) {
             contador++;
             mensaje.append(contador)
                     .append(":")
@@ -68,25 +72,49 @@ public class Contenedor<T extends Producto> {
         var filtrados = new Contenedor<>();
         StringBuilder mensaje = new StringBuilder("Juegos del genero " + genero + ":");
         for (T t : contenedor) {
-            if (t.getGenero() == Genero.valueOf(genero.toUpperCase())) {
+            if (t.getGenero() == Genero.valueOf(genero.toUpperCase())) { // catchear illegalargumentexception
                 filtrados.agregar(t);
             }
         }
         if (filtrados.estaVacio()) {
             throw new ContenedorVacioException("No se encontraron productos con el genero " + genero + ".");
         }
-        return filtrados.toString();
+        return filtrados.mostrarContenedor();
     }
 
     public T buscarProducto(int posicion) throws FormatoInvalidoException, ContenedorVacioException {
-        if(posicion < 0 || posicion > contenedor.size()+1) {
-            throw new FormatoInvalidoException("No se encontro la posicion");
+        if (posicion < 0 || posicion > contenedor.size() + 1) {
+            throw new FormatoInvalidoException("No se encontro el producto");
         }
-        if(contenedor.isEmpty()) {
+        if (contenedor.isEmpty()) {
             throw new ContenedorVacioException("El contenedor esta vacio");
         }
-        return contenedor.get(posicion-1);
+        return contenedor.get(posicion - 1);
     }
 
+    public void modificarTitulo(int posicion, String titulo) throws ContenedorVacioException, FormatoInvalidoException {
+        T producto = buscarProducto(posicion);
+        producto.setTitulo(titulo);
+    }
+
+    public void modificarGenero(int posicion, String genero) throws ContenedorVacioException, FormatoInvalidoException {
+        T producto = buscarProducto(posicion);
+        producto.setGenero(Genero.valueOf(genero.toUpperCase())); // illegalargumentexception
+    }
+
+    public void modificarCreador(int posicion, String creador) throws ContenedorVacioException, FormatoInvalidoException {
+        T producto = buscarProducto(posicion);
+        producto.setCreador(creador);
+    }
+    public void modificarVersion (int posicion, int version) throws FormatoInvalidoException, ContenedorVacioException {
+        T producto = buscarProducto(posicion);
+        if(producto instanceof Juego)
+            ((Juego) producto).setNumeroVersion(version);
+    }
+    public void modificarFecha (int posicion, String fecha) throws ContenedorVacioException, FormatoInvalidoException {
+        T producto = buscarProducto(posicion);
+        if(producto instanceof Expansion)
+            ((Expansion) producto).setFechaLanzamiento(fecha);
+    }
 }
 
--- src/main/java/org/example/UI/App.java
+++ src/main/java/org/example/UI/App.java
@@ -3,4 +3,5 @@ package org.example.UI;
 public class App {
 
 
+
 }
